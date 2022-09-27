0277399bfb132cc46db217dd7778ca2aa7d7c082
nicoghiro <nicolasghirardi251005@gmail.com>
2022-09-27 21:19:03 +0200

    sdfsdfs

--- es2.2022-2023/Form1.Designer.cs
+++ es2.2022-2023/Form1.Designer.cs
@@ -28,21 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "cerca";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(406, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(339, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "inserisci il comune che vuoi cercare ";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(666, 286);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- es2.2022-2023/Form1.cs
+++ es2.2022-2023/Form1.cs
@@ -20,13 +20,60 @@ namespace es2._2022_2023
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string p = textBox1.Text;
+            p.ToUpper();
             int recordlenght = 528;
             var f = new FileStream(@"./veneto_verona.csv", FileMode.Open, FileAccess.ReadWrite);
             int tot = ((int)(f.Length));
-            int linee = tot / recordlenght+1;
+            int linee = tot / recordlenght + 1;
             string line = Convert.ToString(linee);
             MessageBox.Show(line);
             f.Close();
+            comune a = Getstring(linee / 2);
+            int n = linee;
+            int j = linee - 1 , pos = -1, i=0 , m;
+            do
+            {
+                m = (i + j) / 2;
+                if (a[m] == x)
+                    pos = m;
+                else if (a[m] < x)
+                    i = m + 1;
+                else
+                    j = m - 1;
+
+            } while (i <= j && pos == -1);
+
+
+
         }
+        public static comune Getstring(int line)
+        {
+            string linea;
+            byte[] br;
+            var f = new FileStream(@"./veneto_verona.csv", FileMode.Open, FileAccess.ReadWrite);
+            f.Seek(line * 528, SeekOrigin.Begin);
+            BinaryReader reader = new BinaryReader(f);
+            br = reader.ReadBytes(528);
+            linea = Encoding.ASCII.GetString(br, 0, br.Length);
+            string[] com = linea.Split(';');
+            comune post;
+            post.posto = com[0];
+            post.modalità = com[2];
+            return post;
+            
+
+        }
+      public struct comune
+    {
+       public string posto;
+        public string modalità;
     }
+    }
+    
 }
