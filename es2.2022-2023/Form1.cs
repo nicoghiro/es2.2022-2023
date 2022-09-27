0df4267014b58acc85bf7402c3a1251c7224ff00
nicoghiro <nicolasghirardi251005@gmail.com>
2022-09-27 21:51:58 +0200

    fine

--- es2.2022-2023/Form1.cs
+++ es2.2022-2023/Form1.cs
@@ -25,14 +25,14 @@ namespace es2._2022_2023
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string p = textBox1.Text;
-            p.ToUpper();
-            int recordlenght = 528;
+          
+            string x = textBox1.Text.Trim();
+            x.ToUpper();          
+            int recordlenght = 529;
             var f = new FileStream(@"./veneto_verona.csv", FileMode.Open, FileAccess.ReadWrite);
             int tot = ((int)(f.Length));
             int linee = tot / recordlenght + 1;
-            string line = Convert.ToString(linee);
-            MessageBox.Show(line);
+           
             f.Close();
             comune a = Getstring(linee / 2);
             int n = linee;
@@ -40,15 +40,26 @@ namespace es2._2022_2023
             do
             {
                 m = (i + j) / 2;
-                if (a[m] == x)
-                    pos = m;
-                else if (a[m] < x)
+                a=Getstring(m);
+                
+                int result = string.Compare(a.posto, x);
+                if (a.posto == x) { 
+                    MessageBox.Show(a.modalità);
+                    pos++;
+                }
+                
+                else if (result==-1) { 
                     i = m + 1;
-                else
+                }
+                else { 
                     j = m - 1;
+                }
 
             } while (i <= j && pos == -1);
-
+            
+            if (pos == -1) { 
+                MessageBox.Show("Comune non trovato");
+            }
 
 
         }
@@ -57,14 +68,15 @@ namespace es2._2022_2023
             string linea;
             byte[] br;
             var f = new FileStream(@"./veneto_verona.csv", FileMode.Open, FileAccess.ReadWrite);
-            f.Seek(line * 528, SeekOrigin.Begin);
+            f.Seek(line * 529, SeekOrigin.Begin);
             BinaryReader reader = new BinaryReader(f);
-            br = reader.ReadBytes(528);
+            br = reader.ReadBytes(529);
             linea = Encoding.ASCII.GetString(br, 0, br.Length);
             string[] com = linea.Split(';');
             comune post;
             post.posto = com[0];
-            post.modalità = com[2];
+            post.modalità = com[3];
+            f.Close();
             return post;
             
 
